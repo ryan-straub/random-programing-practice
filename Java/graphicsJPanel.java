066a7c233d3a63151c327632bddad0420259aeb4
Ryan Straub <ryan.straub.business1@gmail.com>
2022-07-19 11:17:04 -0700

    Making Calc
    -Making Calc

---
+++ Java/graphicsJPanel.java
@@ -0,0 +1,30 @@
+import javax.swing.*;
+import java.awt.*;
+
+public class graphicsJPanel extends JPanel{
+
+    // public void paint(Graphics g){
+
+    //     g.setColor(Color.CYAN);
+    //     g.drawRect(20, 80, 40, 40);
+    //     g.fillRect(20, 80, 40, 40);
+
+    //     g.setColor(Color.CYAN);
+    //     g.drawRect(80, 80, 40, 40);
+    //     g.fillRect(80, 80, 40, 40);
+
+    //     g.setColor(Color.CYAN);
+    //     g.drawRect(140, 80, 40, 40);
+    //     g.fillRect(140, 80, 40, 40);
+
+    //     g.setColor(Color.CYAN);
+    //     g.drawRect(200, 80, 40, 40);
+    //     g.fillRect(200, 80, 40, 40);
+
+    // }
+
+    // public void button(){
+    //     JButton b = new JButton();
+    //     b.setBounds(50, 100, 95, 30);
+    // }
+}
---
+++ password.java
@@ -0,0 +1,8 @@
+public class password{
+
+    public void User(Graphics g){
+        g.setColor(Color.red);
+        g.drawRect(20, 20, 200, 180);
+        g.fillRect(20, 20, 200, 180);
+    }
+}
